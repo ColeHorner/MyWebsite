563eac37682a992fa4d1b1dd3dc6e9ec636d0295
Cole Horner <colehorner94@gmail.com>
2022-07-16 11:21:29 -0500

    Setting navbar to basic code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,47 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
 import './Header.css';
 
+import React, { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { AiOutlineGithub, AiFillLinkedin } from "react-icons/ai";
+
 function Header(){
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return(
-    <Navbar className="navbar" bg="light" expand="lg">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link 
-              className="text" 
-              href="/" 
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link 
-              className="text" 
-              href="/projects"
-            >
-            Projects
-            </Nav.Link>
-          </Nav>
-          <Nav className="icon">
-            <Nav.Link 
-              target={'_blank'}
-              href='https://github.com/colehorner'
-            >
-              <AiOutlineGithub/>
-            </Nav.Link>
-            <Nav.Link 
-              target={'_blank'}
-              href='https://www.linkedin.com/in/cole-horner/'
-            >
-              <AiFillLinkedin/>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar bg="light">
+        <Container>
+            <Nav>
+              <Nav.Link 
+                className="text" 
+                href="/" 
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link 
+                className="text" 
+                href="/projects"
+              >
+              Projects
+              </Nav.Link>
+            </Nav>
+            <Nav className="icon">
+              <Nav.Link 
+                target={'_blank'}
+                href='https://github.com/colehorner'
+              >
+                <AiOutlineGithub/>
+              </Nav.Link>
+              <Nav.Link 
+                target={'_blank'}
+                href='https://www.linkedin.com/in/cole-horner/'
+              >
+                <AiFillLinkedin/>
+              </Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
